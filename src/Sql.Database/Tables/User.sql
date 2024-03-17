6f02c51e8daba1605d7dfa602760eb00ab8fa7d3
Ksi <labani.said@outlook.com>
2024-03-17 16:55:15 +0100

    Add user table

---
+++ src/Sql.Database/Tables/User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id]			INT				IDENTITY (1,1)	NOT NULL,
+	[FirstName]		VARCHAR(100)					NOT NULL,
+	[LastName]		VARCHAR(100)					NOT NULL,
+	[CurrencyId]	INT								NOT NULL,
+
+	CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC),
+
+	CONSTRAINT [FK_UserCurrency] 
+		FOREIGN KEY ([CurrencyId])
+		REFERENCES [dbo].[Currency]([Id])
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION,
+)
