bfef389d64426cf0161891e7e23f7afdf09be4b6
Ksi <labani.said@outlook.com>
2024-03-17 16:53:39 +0100

    Add Currency table

---
+++ src/Sql.Database/Tables/Currency.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Currency]
+(
+	[Id]		INT			IDENTITY (1,1)		NOT NULL,
+	[Code]		CHAR(3)							NOT NULL,
+    [Name]		NVARCHAR(255)					NOT NULL,
+    [Symbol]	NVARCHAR(10)					NOT NULL,
+
+	CONSTRAINT [PK_Currency] PRIMARY KEY CLUSTERED ([Id] ASC),
+);
+
+
