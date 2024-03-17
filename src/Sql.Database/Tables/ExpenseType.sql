14bf49de273725de34062725e73bb08eeaf9a09b
Ksi <labani.said@outlook.com>
2024-03-17 16:54:23 +0100

    Add expense type

---
+++ src/Sql.Database/Tables/ExpenseType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ExpenseType]
+(
+	[Id]		INT				IDENTITY (1,1)		NOT NULL,
+	[Label]		VARCHAR(MAX)						NOT NULL,
+
+	CONSTRAINT [PK_ExepenseType] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
