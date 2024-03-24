9b280e9eba3f70b4f49d14a1381b7226efd5655c
Ksi <labani.said@outlook.com>
2024-03-25 00:52:13 +0100

    Reduce the cyclomatic number of the method class FindAsync in ExpenseRepository

---
+++ src/Sql/Expenses/ExpenseDataExtensions.cs
@@ -0,0 +1,77 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExpenseDataExtensions.cs" company="WalletSystem">
+//     Copyright (c) WalletSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace WalletSystem.Expenses.Sql
+{
+    using Microsoft.EntityFrameworkCore;
+    using WalletSystem.Sql;
+
+    public static class ExpenseDataExtensions
+    {
+        public static IQueryable<ExpenseData> WhereAmountEquals(this IQueryable<ExpenseData> expenses, decimal? amount)
+        {
+            if (amount.HasValue)
+            {
+                expenses = expenses.Where(e => e.Amount == amount);
+            }
+
+            return expenses;
+        }
+
+        public static IQueryable<ExpenseData> WhereDateEquals(this IQueryable<ExpenseData> expenses, DateTime? date)
+        {
+            if (date.HasValue)
+            {
+                expenses = expenses.Where(e => e.Date == date);
+            }
+
+            return expenses;
+        }
+
+        public static IQueryable<ExpenseData> WhereUserIdEquals(this IQueryable<ExpenseData> expenses, int? userId)
+        {
+            if (userId.HasValue)
+            {
+                expenses = expenses.Where(e => e.UserId == userId);
+            }
+
+            return expenses;
+        }
+
+        public static IQueryable<ExpenseData> Sort(this IQueryable<ExpenseData> expenses, SortBy? sortBy, string? sortOption)
+        {
+            return (sortBy, sortOption?.ToUpper()) switch
+            {
+                (SortBy.Amount, "DESC") => expenses.OrderByDescending(e => e.Amount),
+                (SortBy.Amount, _) => expenses.OrderBy(e => e.Amount),
+                (SortBy.Date, "DESC") => expenses.OrderByDescending(e => e.Date),
+                (SortBy.Date, _) => expenses.OrderBy(e => e.Date),
+                _ => expenses
+            };
+        }
+
+        public static async Task<IReadOnlyCollection<Expense>> ToExpensesAsync(this IQueryable<ExpenseData> expensesQuery)
+        {
+            var expensesData = await expensesQuery.ToArrayAsync();
+
+            var expenseTasks = expensesData.Select(ExpenseModelToExpenseAsync);
+
+            return await Task.WhenAll(expenseTasks);
+        }
+
+        private static async Task<Expense> ExpenseModelToExpenseAsync(ExpenseData model)
+        {
+            var amount = new Amount(model.Amount, currency: (Currency)model.User!.CurrencyId);
+
+            return await Task.FromResult(new Expense(
+                date: model.Date,
+                amount: amount,
+                description: model.Description,
+                user: $"{model.User?.FirstName} {model.User?.LastName}",
+                type: (ExpenseType)model.Type));
+        }
+    }
+}
--- src/Sql/Expenses/ExpenseRepository.cs
+++ src/Sql/Expenses/ExpenseRepository.cs
@@ -28,66 +28,24 @@ namespace WalletSystem.Expenses.Sql
                 type: (int)expenseCreation.Type);
 
             await this.context.Expenses.AddAsync(expenseData);
+
             await this.context.SaveChangesAsync();
         }
 
         public async Task<IReadOnlyCollection<Expense>> FindAsync(ExpenseQuery query)
         {
-            var expenses = this.context.Expenses
+            var expenses = await this.context.Expenses
                 .Include(e => e.User)
                 .ThenInclude(u => u!.Currency)
                 .AsNoTracking()
-                .AsQueryable();
+                .AsQueryable()
+                .WhereAmountEquals(query.Amount)
+                .WhereDateEquals(query.Date)
+                .WhereUserIdEquals(query.UserId)
+                .Sort(query.SortBy, query.SortOption)
+                .ToExpensesAsync();
 
-            if (query.Date.HasValue)
-            {
-                expenses = expenses.Where(e => e.Date.Date == query.Date.Value.Date);
-            }
-
-            if (query.Amount.HasValue)
-            {
-                expenses = expenses.Where(e => e.Amount == query.Amount);
-            }
-
-            if (query.UserId.HasValue)
-            {
-                expenses = expenses.Where(e => e.UserId == query.UserId);
-            }
-
-            if (query.SortBy.HasValue)
-            {
-                if (query.SortBy == SortBy.Amount)
-                {
-                    expenses = query.SortOption?.ToUpper() == "DESC"
-                    ? expenses.OrderByDescending(e => e.Amount)
-                    : expenses.OrderBy(e => e.Amount);
-                }
-
-                if (query.SortBy == SortBy.Date)
-                {
-                    expenses = query.SortOption?.ToUpper() == "DESC"
-                    ? expenses.OrderByDescending(e => e.Date)
-                    : expenses.OrderBy(e => e.Date);
-                }
-            }
-
-            var result = await expenses.ToArrayAsync();
-
-            return result
-                .Select(ExpenseModelToExpense)
-                .ToArray();
-        }
-
-        private static Expense ExpenseModelToExpense(ExpenseData model)
-        {
-            var amount = new Amount(model.Amount, currency: (Currency)model.User!.CurrencyId);
-
-            return new Expense(
-                date: model.Date,
-                amount: amount,
-                description: model.Description,
-                user: $"{model.User?.FirstName} {model.User?.LastName}",
-                type: (ExpenseType)model.Type);
+            return expenses;
         }
     }
 }
